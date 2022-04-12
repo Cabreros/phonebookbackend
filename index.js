188f47d3ab7cd9fefc5448bd57cfac549592e3c7
michael <cabreros17@gmail.com>
2022-04-12 09:16:54 -0400

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(morgan("tiny"), morgan(":person"));
 
 morgan.token("person", (req) => {
@@ -89,6 +91,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
