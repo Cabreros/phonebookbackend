8000816cf11a662408be5e165e0ad64bc4c4a63d
michael <cabreros17@gmail.com>
2022-04-14 09:50:30 -0400

    added connection to mongodb

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.oiipd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  content: {
+    id: Number,
+    name: String,
+    number: String,
+  },
+  date: Date,
+  important: Boolean,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  content: {
+    id: 1,
+    name: process.argv[3],
+    number: process.argv[4],
+  },
+  date: new Date(),
+  important: true,
+});
+
+person.save().then((result) => {
+  console.log(
+    `added ${process.argv[3]} number ${process.argv[4]} to phonebook`
+  );
+  mongoose.connection.close();
+});
